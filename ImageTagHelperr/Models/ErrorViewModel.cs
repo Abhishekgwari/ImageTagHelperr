9f2658d70ff6de903876190e8f7490c9a921a0ed
Abhishek <Abhishekgwari14sep@gmail.com>
2024-12-04 23:56:43 +0530

    Add project files.

---
+++ ImageTagHelperr/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageTagHelperr.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
